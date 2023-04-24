8bf3361b1fc4d0a98d5810accf43390bb6279d63
alibr0wn <alibrown@purdue.edu>
2023-04-23 20:44:02 -0400

    rpi changes

--- src/sno/sno/lib/arduino.py
+++ src/sno/sno/lib/arduino.py
@@ -1,107 +1,77 @@
-
-from enum import Enum
-import os
-import subprocess
-from adafruit_blinka.board.librecomputer.aml_s905x_cc_v1 import *
-from digitalio import DigitalInOut, Direction
+import RPi.GPIO as GPIO
 import time
 
-class IOType(Enum):
-    OUTPUT = 1
-    INPUT = 0
+# Set up the GPIO pins
+GPIO.setmode(GPIO.BOARD)
 
-class IOValue(Enum):
-    HIGH = 1
-    LOW = 0
-
-def setupPWM():
-    commands = [
-        'echo pi | sudo -S echo sudo_login',
-        'sudo ldto enable pwm-a',
-        'sudo ldto enable pwm-ao-9',
-        'sudo ldto enable pwm-ef',
-        'echo 0 | sudo tee /sys/class/pwm/pwmchip0/export',
-        'echo 0 | sudo tee /sys/class/pwm/pwmchip2/export',
-        'echo 1 | sudo tee /sys/class/pwm/pwmchip2/export',
-        'echo 0 | sudo tee /sys/class/pwm/pwmchip4/export',
-        'echo 1 | sudo tee /sys/class/pwm/pwmchip4/export'
-    ]
-    for command in commands:
-        subprocess.run(command, stderr=subprocess.PIPE, stdout=subprocess.DEVNULL, shell=True)
-setupPWM() 
-
-gpio_pins = {8: P8, 
-             10: P10, 
-             16: P16, 
-             18: P18, 
-             22: P22,
-             24: P24,
-             26: P26,
-             36: P36,
-             38: P38, 
-             40: P40, 
-             19: P19, 
-             21: P21, 
-             23: P23, 
-             29: P29, 
-             31: P31, 
-             37: P37}
+gpio_pins = {
+    8: 10,
+    10: 19,
+    16: 36,
+    18: 12,
+    22: 15,
+    24: 18,
+    26: 11,
+    36: 16,
+    38: 20,
+    40: 21,
+    19: 35,
+    21: 29,
+    23: 33,
+    29: 31,
+    31: 26,
+    37: 13
+}
 
 pwm_pins = {
-    11: (4, 0), # 11086c0 pwm0 pwmchip4   11
-    13: (4, 1), # 11086c0 pwm1 pwmchip4   13
-    32: (2, 0), # 8100550 pwm0 pwmchip2   32
-    33: (0, 0), # 1108550 pwm0 pwmchip0   33
-    35: (2, 1), # 8100550 pwm1 pwmchip2   35
+    11: 17,
+    13: 27,
+    32: 12,
+    33: 13,
+    35: 19
 }
+
+for pin in pwm_pins.values():
+    GPIO.setup(pin, GPIO.OUT)
+    pwm = GPIO.PWM(pin, 1000)
+    pwm.start(0)
 
 def getPin(pin:int):
     if pin in pwm_pins:
         return pin
     elif pin in gpio_pins:
-        return DigitalInOut(gpio_pins[pin])
+        return gpio_pins[pin]
     raise Exception('not a pin')
 
-
-def pinMode(pin:DigitalInOut, iotype:IOType):
-    if isinstance(pin,DigitalInOut):
-        pin.direction = Direction.OUTPUT if iotype.value == IOType.OUTPUT.value else Direction.INPUT
+def pinMode(pin:int, iotype:int):
+    if iotype == IOType.OUTPUT.value:
+        GPIO.setup(pin, GPIO.OUT)
+    elif iotype == IOType.INPUT.value:
+        GPIO.setup(pin, GPIO.IN)
     else:
-        print(f'not a digital pin {pin}')
+        raise ValueError('Invalid IOType')
 
-def digitalWrite(pin:DigitalInOut, iovalue:int) -> None:
-    if isinstance(pin, DigitalInOut):
-        pin.value = 1 if iovalue == IOValue.HIGH.value else 0
+def digitalWrite(pin:int, iovalue:int):
+    if iovalue == IOValue.HIGH.value:
+        GPIO.output(pin, GPIO.HIGH)
+    elif iovalue == IOValue.LOW.value:
+        GPIO.output(pin, GPIO.LOW)
     else:
-        raise Exception('digitalWrite: not a digital pin')
+        raise ValueError('Invalid IOValue')
 
-def digitalRead(pin:DigitalInOut) -> bool:
-    if not isinstance(pin, DigitalInOut):
-        raise Exception('digitalRead: not a digital pin')
-    if pin.direction != Direction.INPUT:
-        raise Exception('digitalRead: pin in output mode')
-    return pin.value
+def digitalRead(pin:int) -> bool:
+    return GPIO.input(pin)
 
 def analogWrite(pin:int, value:int):
-    ''' @value [0, 1] '''
     if pin not in pwm_pins:
         raise Exception('analogWrite: no pwm on pin')
     if value > 1 or value < 0:
         raise Exception(f'analogWrite: value should be [0,1], is {value}')
-    
-    chip, channel = pwm_pins[pin]
-    path = f"/sys/class/pwm/pwmchip{chip}/pwm{channel}"
-    period = 1000000 # int(1/frequency * 1000000000)
-    duty_cycle = int(period * value)
-    commands = [
-        f'echo pi | sudo -S echo sudo_login',
-        f'echo 0 | sudo tee {path}/enable',
-        f'echo {period} | sudo tee {path}/period',
-        f'echo {duty_cycle} | sudo -S tee {path}/duty_cycle',
-        f'echo 1 | sudo -S tee {path}/enable'
-    ]
-    for command in commands:
-        subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, shell=True)
+
+    duty_cycle = value * 100
+    GPIO.output(pwm_pins[pin], GPIO.HIGH)
+    pwm = GPIO.PWM(pwm_pins[pin], 1000)
+    pwm.start(duty_cycle)
 
 def micros():
     return time.time_ns() / 1e3 # time us
@@ -114,4 +84,4 @@ def delayMicroseconds(microseconds):
             break
 
 def delay(ms):
-    delayMicroseconds(ms*1000)
+    time.sleep(ms/1000)
